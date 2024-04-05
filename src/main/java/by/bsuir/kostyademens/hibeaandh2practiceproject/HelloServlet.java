fecf92365859024fdbb6b3a9912e60b05b227ba6
KostyaDemens <kostyademens@gmail.com>
2024-04-05 20:11:16 +0300

    html + css

--- src/main/java/by/bsuir/kostyademens/hibeaandh2practiceproject/HelloServlet.java
+++ src/main/java/by/bsuir/kostyademens/hibeaandh2practiceproject/HelloServlet.java
@@ -1,27 +1,16 @@
 package by.bsuir.kostyademens.hibeaandh2practiceproject;
 
 import java.io.*;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("view/main.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/view/main.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<style>
+    <%@ include file="/view/css/commos.css" %>
+</style>
+<style>
+    <%@ include file="/view/css/main.css" %>
+</style>
+<head>
+    <title>TENNIS</title>
+</head>
+<body>
+<div class="main_container">
+    <div class = "header">
+        <h1>Большой теннис</h1>
+    </div>
+    <div class="links_container">
+<%--        <a href="${pageContext.request.contextPath}/new-match">Новый матч</a>--%>
+<%--        <a href="${pageContext.request.contextPath}/matches">Сыгранные матчи</a>--%>
+    </div>
+</div>
+</body>
+</html>
